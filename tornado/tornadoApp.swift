bf30cb8ea3bf77a1048f6650e5badee8dd81625b
iustiuc <iustiuc.principal@gmail.com>
2025-06-27 18:34:08 +0300

    1: Creating the project in `Xcode`

---
+++ tornado/tornadoApp.swift
@@ -0,0 +1,17 @@
+//
+//  tornadoApp.swift
+//  tornado
+//
+//  Created by Ionuț-Daniel Iuștiuc on 27.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct tornadoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
